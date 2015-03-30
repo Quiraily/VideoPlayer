2382a7a3c71d56ed9f93b37351c9b59040a3fa19
Kunpeng Feng <moeacgfan@gmail.com>
2015-03-30 17:15:30 +0200

    Player class update

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// var player = new Player();
+
+// player.init({
+// 	'el':'player',
+// 	'source':'data/video.mp4'
+// });
--- js/module/Player.js
+++ js/module/Player.js
@@ -1,13 +1,23 @@
-$(function(){
-
-var Player = function () {
-
+var Player = function(){
+	this.el;
+	this.source;
 };
+
+
+Player.prototype.ui = function(el,source){
+	this.el.append('<video id="v_'+this.el.attr('id')+'" width="400" height="280"><source src="'+option.source+'"></video>');
+}
 
 Player.prototype.play = function() {
-
+	$('#v_'+this.el.attr('id')).get(0).play();
 };
 
+Player.prototype.pause = function() {
+	$('#v_'+this.el.attr('id')).get(0).pause();
+};
 
-
-});
+Player.prototype.init = function(option){
+	this.el = $('#'+option.el);
+	this.source = option.source;
+	this.ui(this.el, this.source);
+}
